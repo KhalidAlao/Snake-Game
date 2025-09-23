5dfd15e3a8e252dba68fce803d7ea3f9f43e15a9
KhalidAlao <alaokha@gmail.com>
2025-09-23 21:15:32 +0100

    add logic module skeleton

---
+++ src/logic.js
@@ -0,0 +1,19 @@
+export function initGame() {
+
+};
+
+export function getState() {
+
+};
+
+export function step() {
+
+};
+
+export function changeDirection() {};
+
+export function reset() {};
+
+export function setDimensions() {};
+
+export function registerListener() {};
