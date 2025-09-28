254f718feba6092d9aaf089bb1e10aa49b48b255
KhalidAlao <alaokha@gmail.com>
2025-09-28 23:10:09 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@
 
 export const GRID_SIZE = 15;
 export const BASE_GAME_SPEED = 100; // initial game speed
+export const MIN_MOVE_INTERVAL = 50; // minimum interval so snake never moves too fast
 
 export const FOOD_POINT = 5;
 
