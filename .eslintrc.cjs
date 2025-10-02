a6ecdc192237df16c1397b73c1a1a31caffed2ae
KhalidAlao <alaokha@gmail.com>
2025-10-02 21:19:30 +0100

    Resolve Eslint and Prettier conflict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,28 @@
 module.exports = {
-  root: true,
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false, // allows parsing without a Babel config
-    ecmaVersion: 2023, // latest ECMAScript features
-    sourceType: 'module', // for import/export
-  },
-  extends: ['airbnb-base'],
-  env: {
-    browser: true,
-    node: true,
-    es2023: true,
-  },
-  rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'always', // allow .js extension for local JS files
-      },
+    root: true,
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      requireConfigFile: false,
+      ecmaVersion: 2023,
+      sourceType: 'module',
+    },
+    extends: [
+      'airbnb-base',
+      'plugin:prettier/recommended', 
     ],
-  },
-};
+    env: {
+      browser: true,
+      node: true,
+      es2023: true,
+    },
+    rules: {
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'always',
+        },
+      ],
+      // Add any specific rule overrides here if needed
+    },
+  };
