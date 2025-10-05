c21b18be40878961d4a20333548764724f57bb43
KhalidAlao <alaokha@gmail.com>
2025-10-05 23:08:10 +0100

    Update LeaderboardController.java

--- backend/src/main/java/com/controller/LeaderboardController.java
+++ backend/src/main/java/com/controller/LeaderboardController.java
@@ -1,5 +1,30 @@
 package com.controller;
 
+import com.model.LeaderboardEntry;
+import com.service.LeaderboardService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/leaderboard")
 public class LeaderboardController {
-    
+
+    private final LeaderboardService leaderboardService;
+
+    public LeaderboardController(LeaderboardService leaderboardService) {
+        this.leaderboardService = leaderboardService;
+    }
+
+    // GET top leaderboard
+    @GetMapping
+    public List<LeaderboardEntry> getLeaderboard() {
+        return leaderboardService.getTopEntries();
+    }
+
+    // POST new entry
+    @PostMapping
+    public void addEntry(@RequestBody LeaderboardEntry entry) {
+        leaderboardService.addEntry(entry.getName(), entry.getScore());
+    }
 }
