0fe1463cf55f6989b37837f504f769df856db264
KhalidAlao <alaokha@gmail.com>
2025-10-05 19:44:42 +0100

    Create LeaderboardEntry.java

---
+++ backend/src/main/java/com/model/LeaderboardEntry.java
@@ -0,0 +1,22 @@
+package com.model;
+
+public class LeaderboardEntry {
+
+    private String name;
+    private int score;
+
+    public LeaderboardEntry(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    
+}
